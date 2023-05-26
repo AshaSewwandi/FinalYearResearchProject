5bc4182bc30cffffe15f55918aae49658f77aff3
Fyp98@98 <lakminiprasadi00@gmail.com>
2023-05-26 20:27:23 +0530

    create homepage

---
+++ resources/views/cdn.blade.php
@@ -0,0 +1,36 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<style>
+    .form-group label {
+        text-align: left;
+        display: block;
+    }
+
+    .upload-card {
+      margin-top: 20px;
+    }
+
+
+    .upload-card {
+      margin-top: 20px;
+    }
+    .upload-card .card-label {
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+
+
+    .custom-line-spacing li {
+  margin-bottom: 10px; /* Adjust this value to increase/decrease line spacing */
+}
+
+.custom-line-spacing li {
+    padding-bottom: 0.02px; /* Adjust this value to increase/decrease line spacing */
+  }
+
+  .custom-line-spacing li {
+    padding-bottom: 0.1px; 
+  }
+  .custom-line-spacing .no-spacing {
+    padding-bottom: 0;
+  }
+</style>
